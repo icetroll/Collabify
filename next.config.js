312be64afb9530a913991a206e63d5c15cfd667c
Icey <wowdannef@hotmail.com>
2022-09-25 16:10:57 +0200

    fixed api

--- next.config.js
+++ next.config.js
@@ -7,6 +7,6 @@ module.exports = {
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
+            : 'https://collabify-oag5.vercel.app/api' // production api
     }
 }
